acc2026ae632d59b0b7a34650da838f403baf8a7
Jaqueline <heydi.pinto@cbtis72.edu.mx>
2019-04-23 15:40:58 -0600

    Avances

---
+++ app/src/main/java/com/example/heydi/tips_de_salud/FragmentoListaArticulos.java
@@ -0,0 +1,29 @@
+package com.example.heydi.tips_de_salud;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentoListaArticulos extends Fragment {
+
+
+    public FragmentoListaArticulos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragmento_lista_articulos, container, false);
+    }
+
+}
