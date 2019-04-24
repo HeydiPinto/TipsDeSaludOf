10b9eba1a7f6b529830efd33156e755c75389a15
Jaqueline <heydi.pinto@cbtis72.edu.mx>
2019-04-23 22:48:14 -0600

    Avances

--- app/src/main/java/com/example/heydi/tips_de_salud/ActividadListaArticulos.java
+++ app/src/main/java/com/example/heydi/tips_de_salud/ActividadListaArticulos.java
@@ -7,28 +7,62 @@ import android.support.v7.widget.Toolbar;
 
 import com.example.heydi.tips_de_salud.modeloA.ModeloArticulos;
 
+
+
 /**
  * Actividad con la lista de artículos. Si el ancho del dispositivo es mayor o igual a 900dp, entonces
  * se incrusta el fragmento de detalle {@link FragmentoDetalleArticulo} para generar el patrón
  * Master-detail
  */
+public class ActividadListaArticulos extends AppCompatActivity
+        implements FragmentoListaArticulos.EscuchaFragmento {
+
+    // ¿Hay dos paneles?
+    private boolean dosPaneles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_lista_articulos);
+
+        ((Toolbar) findViewById(R.id.toolbar)).setTitle(getTitle());
+
+        // Verificación: ¿Existe el detalle en el layout?
+        if (findViewById(R.id.contenedor_detalle_articulo) != null) {
+            // Si es asi, entonces confirmar modo Master-Detail
+            dosPaneles = true;
+
+            cargarFragmentoDetalle(ModeloArticulos.ITEMS.get(0).id);
+        }
+
+        // Agregar fragmento de lista
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contenedor_lista, FragmentoListaArticulos.crear())
+                .commit();
+
+    }
+
+    private void cargarFragmentoDetalle(String id) {
+        Bundle arguments = new Bundle();
+        arguments.putString(FragmentoDetalleArticulo.ID_ARTICULO, id);
+        FragmentoDetalleArticulo fragment = new FragmentoDetalleArticulo();
+        fragment.setArguments(arguments);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contenedor_detalle_articulo, fragment)
+                .commit();
+    }
 
 
+    @Override
+    public void alSeleccionarItem(String idArticulo) {
+        if (dosPaneles) {
+            cargarFragmentoDetalle(idArticulo);
+        } else {
+            Intent intent = new Intent(this, ActividadDetalleArticulo.class);
+            intent.putExtra(FragmentoDetalleArticulo.ID_ARTICULO, idArticulo);
 
-public class ActividadListaArticulos extends AppCompatActivity implements FragmentoListaArticulos.EscuchaFragmento {
-
-@Override
-protected void onCreate(Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.actividad_lista_articulos);
-
-    ((Toolbar) findViewById(R.id.toolbar)).setTitle(getTitle());
-
-    //Agregar fragmento de lista
-    getSupportFragmentManager().beginTransaction().replace(R.id.contenedor_lista, FragmentoListaArticulos.crear()).commit();
-}
-@Override
-    public void alSeleccionarItem(String idArticulo){
-
-}
+            startActivity(intent);
+        }
+    }
 }
