e3f1769ba0b186301b35aa8304345f540207e88f
Jaqueline <heydi.pinto@cbtis72.edu.mx>
2019-04-23 22:32:35 -0600

    Avances

--- app/src/main/java/com/example/heydi/tips_de_salud/FragmentoDetalleArticulo.java
+++ app/src/main/java/com/example/heydi/tips_de_salud/FragmentoDetalleArticulo.java
@@ -1,29 +1,61 @@
 package com.example.heydi.tips_de_salud;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.heydi.tips_de_salud.modeloA.ModeloArticulos;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Fragmento que representa el panel del detalle de un artículo.
  */
 public class FragmentoDetalleArticulo extends Fragment {
 
+    // EXTRA
+    public static final String ID_ARTICULO = "extra.idArticulo";
+
+    // Articulo al cuál está ligado la UI
+    private ModeloArticulos.Articulo itemDetallado;
 
     public FragmentoDetalleArticulo() {
-        // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ID_ARTICULO)) {
+            // Cargar modelo según el identificador
+            itemDetallado = ModeloArticulos.MAPA_ITEMS.get(getArguments().getString(ID_ARTICULO));
+
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmento_detalle_articulo, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragmento_detalle_articulo, container, false);
 
+        if (itemDetallado != null) {
+            // Toolbar en master-detail
+            Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar_detalle);
+            if (toolbar != null)
+                toolbar.inflateMenu(R.menu.menu_actividad_lista_articulos);
+
+            ((TextView) v.findViewById(R.id.titulo)).setText(itemDetallado.titulo);
+            ((TextView) v.findViewById(R.id.fecha)).setText(itemDetallado.fecha);
+            ((TextView) v.findViewById(R.id.contenido)).setText(getText(R.string.lorem));
+            Glide.with(this)
+                    .load(itemDetallado.urlMiniatura)
+                    .into((ImageView) v.findViewById(R.id.imagen));
+        }
+
+        return v;
+    }
 }
